457abb37552d68ed1517fc4a4709642bacc48abf
Trương Thị Thanh Huyền <ht79247@gmail.com>
2026-02-02 18:22:12 +0700

    Add XML Documentation for the DependencyInjection

--- Infrastructure/Configurations/DependencyInjection.cs
+++ Infrastructure/Configurations/DependencyInjection.cs
@@ -12,8 +12,17 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Configurations
 {
+    /// <summary>
+    /// Provides extension methods for configuring infrastructure-related services and dependency injection container.
+    /// </summary>
     public static class DependencyInjection
     {
+        /// <summary>
+        /// Registers the infrastructure layer dependencies, including Database Context, Repositories, and Domain Services.
+        /// </summary>
+        /// <param name="services">The service collection to add the services to.</param>
+        /// <param name="configuration">The configuration instance to retrieve connection strings and settings.</param>
+        /// <returns>The modified <see cref="IServiceCollection"/>.</returns>
         public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("DefaultConnection");
@@ -32,12 +41,10 @@ namespace Infrastructure.Configurations
             services.AddScoped<IJwtService, JwtService>();
             services.AddScoped<IRedisService, RedisService>();
             services.AddScoped<IPaymentRepository, PaymentRepository>();
-            services.AddScoped<IPaymentService, PaymentService>();  
+            services.AddScoped<IPaymentService, PaymentService>();
 
 
             services.AddScoped<IUserRepository, UserRepository>();
-
-
 
             return services;
         }
