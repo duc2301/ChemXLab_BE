e355d78bcbce1c9ca37e22e423a3086bc601d17a
Trương Thị Thanh Huyền <ht79247@gmail.com>
2026-02-02 18:04:25 +0700

    Add XML Documentation for MappingProfile

--- Application/Mapping/MappingProfile.cs
+++ Application/Mapping/MappingProfile.cs
@@ -1,5 +1,4 @@
-﻿
-using Application.DTOs.RequestDTOs.Auth;
+﻿using Application.DTOs.RequestDTOs.Auth;
 using Application.DTOs.RequestDTOs.Package;
 using Application.DTOs.RequestDTOs.Payment;
 using Application.DTOs.ResponseDTOs.Package;
@@ -11,16 +10,25 @@ using System.Text.Json;
 
 namespace Application.Mapping
 {
+    /// <summary>
+    /// Defines the AutoMapper configuration profiles for mapping between Domain Entities and Data Transfer Objects (DTOs).
+    /// </summary>
     public class MappingProfile : Profile
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MappingProfile"/> class and creates the object mapping rules.
+        /// </summary>
         public MappingProfile()
         {
+            // User Mappings
             CreateMap<User, UserResponseDTO>().ReverseMap();
             CreateMap<RegisterDTO, User>().ReverseMap();
+
+            // Payment Mappings
             CreateMap<PaymentTransaction, PaymentResponseDTO>().ReverseMap();
             CreateMap<CreatePaymentDTO, PaymentTransaction>().ReverseMap();
 
-            // Package
+            // Package Mappings
             CreateMap<CreatePackageDTO, Package>()
             .ForMember(dest => dest.Features, opt => opt.MapFrom(src =>
                 src.Features != null ? JsonSerializer.Serialize(src.Features, (JsonSerializerOptions?)null) : null));
