e26486d5822a7f0abfb95c4ed4daff0b9881925b
Trương Thị Thanh Huyền <ht79247@gmail.com>
2026-02-02 18:13:26 +0700

    Add XML Documentation for PresentationExtensions

--- ChemXLabWebAPI/Extensions/PresentationExtensions.cs
+++ ChemXLabWebAPI/Extensions/PresentationExtensions.cs
@@ -3,8 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ChemXLabWebAPI.Extensions
 {
+    /// <summary>
+    /// Provides extension methods for configuring presentation layer services and behaviors.
+    /// </summary>
     public static class PresentationExtensions
     {
+        /// <summary>
+        /// Configures global validation error handling to return a standardized API response format.
+        /// </summary>
+        /// <param name="services">The service collection to add services to.</param>
+        /// <param name="configuration">The application configuration properties.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
         public static IServiceCollection AddGlobalValidation(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<ApiBehaviorOptions>(options =>
