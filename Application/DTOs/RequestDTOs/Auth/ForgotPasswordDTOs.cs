d4ea654c64bd5deeb5a3ec3926a43733a40fe912
Trương Thị Thanh Huyền <ht79247@gmail.com>
2026-02-06 17:48:35 +0700

    add request dtos for authentication and user management

---
+++ Application/DTOs/RequestDTOs/Auth/ForgotPasswordDTOs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Application.DTOs.RequestDTOs.Auth
+{
+    public class ForgotPasswordRequestDTO
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+    }
+
+    public class VerifyOtpDTO
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string OtpCode { get; set; } = null!;
+    }
+
+    public class ResetPasswordDTO
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string OtpCode { get; set; } = null!; 
+        [Required, MinLength(6)]
+        public string NewPassword { get; set; } = null!;
+        [Compare("NewPassword")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
