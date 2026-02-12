304ae24ed6477eb4ef04ff48cb6fdbe66123570b
Trương Thị Thanh Huyền <ht79247@gmail.com>
2026-02-12 23:16:47 +0700

    add DTOs and interfaces for Chemical CRUD

---
+++ Application/Interfaces/IServices/IChemicalService.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.RequestDTOs.Chemical;
+using Application.DTOs.ResponseDTOs.Chemical;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.IServices
+{
+    public interface IChemicalService
+    {
+        Task<IEnumerable<ChemicalResponseDTO>> GetAllChemicalsAsync();
+        Task<ChemicalResponseDTO?> GetChemicalByIdAsync(Guid id);
+        Task<ChemicalResponseDTO> CreateChemicalAsync(CreateChemicalDTO createDto);
+        Task<ChemicalResponseDTO> UpdateChemicalAsync(Guid id, UpdateChemicalDTO updateDto);
+        Task<bool> DeleteChemicalAsync(Guid id);
+    }
+}
